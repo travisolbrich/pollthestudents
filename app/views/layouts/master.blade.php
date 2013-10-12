19a29c13365178681e16f395ae86d0677b2e38e9
Travis Olbrich <travis.olbrich@outlook.com>
2013-10-12 00:03:42 -0500

    Templating

---
+++ app/views/layouts/master.blade.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Template &middot; Bootstrap</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Le styles -->
+    {{ HTML::style('assets/bootstrap/css/bootstrap.css') }} 
+    
+    <style type="text/css">
+      body {
+        padding-top: 20px;
+        padding-bottom: 40px;
+      }
+
+      /* Custom container */
+      .container-narrow {
+        margin: 0 auto;
+        max-width: 700px;
+      }
+      .container-narrow > hr {
+        margin: 30px 0;
+      }
+
+      /* Main marketing message and sign up button */
+      .jumbotron {
+        margin: 60px 0;
+        text-align: center;
+      }
+      .jumbotron h1 {
+        font-size: 72px;
+        line-height: 1;
+      }
+      .jumbotron .btn {
+        font-size: 21px;
+        padding: 14px 24px;
+      }
+
+      /* Supporting marketing content */
+      .marketing {
+        margin: 60px 0;
+      }
+      .marketing p + h4 {
+        margin-top: 28px;
+      }
+      .hidden {
+        visibility: hidden;
+      }
+    </style>
+
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="../assets/js/html5shiv.js"></script>
+    <![endif]-->
+
+  </head>
+
+  <body>
+    
+    @yield('pageContent')
+
+    <!-- Le javascript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    {{ HTML::script('assets/javascript/jQuery.js') }} 
+    {{ HTML::script('assets/javascript/poll.js') }} 
+    {{ HTML::script('assets/bootstrap/js/bootstrap.min.js') }} 
+  </body>
+</html>
--- app/views/polls/create.blade.php
+++ app/views/polls/create.blade.php
@@ -1,84 +1,79 @@
+@extends('layouts.master')
+
+@section('pageContent')
 <h1>Create a Poll</h1>
 
-{{ Form::open(array('url' => 'poll')) }}
 
-{{ Form::label('name', 'Poll Name') }}
-{{ Form::text('name') }}
+<div id="create">            
+    <div class="header">
+        <h1>New Poll</h1>
+    </div>            
+    <div class="poll-info">
+        <label>Poll Prompt</label>
+        <input id="prompt" type="text" placeholder="Where is a fun place to go at night?" checked/>
+        
+        <label>Make Public</label>
+        <input id="isPublic" type="checkbox" checked/>
+    </div>
+    <button id="submit">Submit Poll</button>
+</div>
 
-{{ Form::close() }}
+<div id="success" class="hidden">            
+    <div class="header">
+        <h1>Success!</h1>
+    </div>            
+    <div class="poll-info">
+        <p>Your new pool has been created!</p>
+        <a class="share" href="/poll/view?id=">Share your Poll</a>
+    </div>
+</div>
 
-        <script src="../assets/javascript/jQuery.js"></script>
-        <script src="../assets/javascript/poll.js"></script>
-
-        <div id="create">            
-            <div class="header">
-                <h1>New Poll</h1>
-            </div>            
-            <div class="poll-info">
-                <label>Poll Prompt</label>
-                <input id="prompt" type="text" placeholder="Where is a fun place to go at night?" checked/>
-                
-                <label>Make Public</label>
-                <input id="isPublic" type="checkbox" checked/>
-            </div>
-            <button id="submit">Submit Poll</button>
-        </div>
-
-        <div id="success" class="hidden">            
-            <div class="header">
-                <h1>Success!</h1>
-            </div>            
-            <div class="poll-info">
-                <p>Your new pool has been created!</p>
-                <a class="share" href="/poll/view?id=">Share your Poll</a>
-            </div>
-        </div>
-
-        <script type="text/javascript">
-            
-            var poll = new POLL.MODEL.Poll(),
-                pollRegistry = new POLL.REGISTRY.PollRegistry(),
-                hideCreate,
-                showSuccess,
-                pollSuccess,
-                pollFailure;
-            
-            $( "#isPublic" ).change(function() {
-                
-                var value = $( "#isPublic" ).val();
-                poll.setPrompt(value);
-            });
-            
-            $( "#prompt" ).change(function() {
-                
-                var value = $( "#prompt" ).val();
-                poll.setPrompt(value);
-            });
-            
-            $( "#submit" ).change(function() {
-                pollRegistry.create(poll, pollSuccess, pollFailure);
-            });
-            
-            hideCreate = function () {
-                
-                //$("#success").
-                
-            };
-            
-            showSuccess = function () {
-                
-            };
-            
-            pollSuccess = function (html) {
-              alert('successful : ' + html);
-                //Share your poll here or whatever.
-              $("#result").html("Successful");
-            };
-            
-            pollError = function(data, errorThrown){
-              alert('request failed :'+errorThrown);
-            };
-            
-        </script>
+<script type="text/javascript">
+    
+    var poll = new POLL.MODEL.Poll(),
+        pollRegistry = new POLL.REGISTRY.PollRegistry(),
+        hideCreate,
+        showSuccess,
+        pollSuccess,
+        pollFailure;
+    
+    $( "#isPublic" ).change(function() {
+        
+        var value = $( "#isPublic" ).val();
+        poll.setPrompt(value);
+    });
+    
+    $( "#prompt" ).change(function() {
+        
+        var value = $( "#prompt" ).val();
+        poll.setPrompt(value);
+    });
+    
+    $( "#submit" ).change(function() {
+        pollRegistry.create(poll, pollSuccess, pollFailure);
+    });
+    
+    hideCreate = function () {
+        
+        //$("#success").
+        
+    };
+    
+    showSuccess = function () {
+        
+    };
+    
+    pollSuccess = function (html) {
+      alert('successful : ' + html);
+        //Share your poll here or whatever.
+      $("#result").html("Successful");
+    };
+    
+    pollError = function(data, errorThrown){
+      alert('request failed :'+errorThrown);
+    };
+    
+</script>
 
 
+@stop
