185aca61d5823f200eb79f7a212efad86177c5a3
Derek Burgman <dereekb@gmail.com>
2013-10-12 00:54:12 -0500

    Updated Create Page
    - Updated create page.

--- app/views/polls/create.blade.php
+++ app/views/polls/create.blade.php
@@ -1,79 +1,140 @@
 @extends('layouts.master')
 
 @section('pageContent')
+
 <h1>Create a Poll</h1>
-
-
 <div id="create">            
     <div class="header">
         <h1>New Poll</h1>
     </div>            
     <div class="poll-info">
         <label>Poll Prompt</label>
-        <input id="prompt" type="text" placeholder="Where is a fun place to go at night?" checked/>
+        <input id="prompt" type="text" placeholder="What is your favorite place to eat?" checked/>
         
         <label>Make Public</label>
         <input id="isPublic" type="checkbox" checked/>
     </div>
+    <div class="poll-choices">
+        <ul class="list">
+            <li class="reference choice hidden">
+                <input class="string" type="text" placeholder="Antonio's Pizza" checked/>
+            </li>
+        </ul>
+        <button id="newChoice">New Choice</button>
+    </div>
+    
     <button id="submit">Submit Poll</button>
+</div>
+
+<div id="error" class="hidden">            
+    <div class="header">
+        <h1>Woops!</h1>
+    </div>            
+    <div id="poll-error">
+        <p>An error occured. Try something different...</p>
+    </div>
 </div>
 
 <div id="success" class="hidden">            
     <div class="header">
         <h1>Success!</h1>
     </div>            
-    <div class="poll-info">
+    <div id="poll-info">
         <p>Your new pool has been created!</p>
-        <a class="share" href="/poll/view?id=">Share your Poll</a>
+        <a id="newPollLink" href="/poll/view?id=">Share your Poll</a>
     </div>
 </div>
 
 <script type="text/javascript">
     
-    var poll = new POLL.MODEL.Poll(),
-        pollRegistry = new POLL.REGISTRY.PollRegistry(),
-        hideCreate,
-        showSuccess,
-        pollSuccess,
-        pollFailure;
-    
-    $( "#isPublic" ).change(function() {
+    $(document).ready(function() {
         
-        var value = $( "#isPublic" ).val();
-        poll.setPrompt(value);
+        var poll = new POLL.MODEL.Poll(),
+            pollRegistry = new POLL.REGISTRY.PollRegistry(),
+            createChoice,
+            hideCreate,
+            showSuccess,
+            showError,
+            pollSuccess,
+            pollFailure;
+        
+        $( "#isPublic" ).change(function() {
+            
+            var value = $( "#isPublic" ).val();
+            poll.setPrompt(value);
+        });
+        
+        $( "#prompt" ).change(function() {
+            var value = $( "#prompt" ).val();
+            poll.setPrompt(value);
+        });
+        
+        $( "#newChoice" ).click(function() {
+            createChoice();
+        });
+        
+        $( "#submit" ).click(function() {
+            
+            //Create options for each option.
+            var choices = [], 
+                choiceListing = $(".poll-choices .list");
+    
+            $(this).find('li').each(function(){
+                // cache jquery var
+                var current = $(this);
+                
+                if(current.hasClass("hidden") == false)
+                {
+                    var choice = new POLL.MODEL.Choice(),
+                        value = current.find("string");
+                    
+                    if(value !== ""){
+                        choice.setName(value);
+                        choices.push(choice);
+                    }
+                }
+            });
+            
+            poll.setChoices(choices);
+            pollRegistry.create(poll, pollSuccess, pollFailure);
+        });
+        
+        createChoice = function () {
+          
+            var choiceList = $(".poll-choices .list"),
+                choiceReference = choiceList.find(".reference"),
+                clone = choiceReference.clone(true);
+            
+            clone.toggleClass("hidden");
+            clone.toggleClass("reference");
+            clone.appendTo(choiceList);
+        };
+                
+        hideCreate = function () {
+            $("#create").toggleClass("hidden");
+        };
+        
+        showSuccess = function (pollId) {
+            $("#success").toggleClass("hidden");
+            hideCreate();
+            
+            var address = "/poll/" + pollId;
+            $('newPollLink').attr('href',('/poll/view?id='+pollId));
+        };
+        
+        showError = function () {
+            $("#error").toggleClass("hidden");
+        };
+        
+        pollSuccess = function (html) {
+            showSuccess();
+        };
+        
+        pollError = function(data, errorThrown){
+            showError();
+        };
+        
     });
-    
-    $( "#prompt" ).change(function() {
-        
-        var value = $( "#prompt" ).val();
-        poll.setPrompt(value);
-    });
-    
-    $( "#submit" ).change(function() {
-        pollRegistry.create(poll, pollSuccess, pollFailure);
-    });
-    
-    hideCreate = function () {
-        
-        //$("#success").
-        
-    };
-    
-    showSuccess = function () {
-        
-    };
-    
-    pollSuccess = function (html) {
-      alert('successful : ' + html);
-        //Share your poll here or whatever.
-      $("#result").html("Successful");
-    };
-    
-    pollError = function(data, errorThrown){
-      alert('request failed :'+errorThrown);
-    };
-    
 </script>
-
 
 @stop
