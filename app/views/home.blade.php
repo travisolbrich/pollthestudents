5fbf9953a39043882511c52a80486a88af645343
Derek Burgman <dereekb@gmail.com>
2013-10-12 15:19:57 -0500

    Updated Home

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -5,16 +5,95 @@ Poll Something New
 @stop
 
 @section('pageContent')
-    <div class="container-narrow">
+<div class="container-narrow">
 
-      @include('includes.masthead')
+  @include('includes.masthead')
 
-      <div class="jumbotron">
-        <h1>Create a Poll!</h1>
-        <p class="lead">Create a new public poll.</p>
-        <a class="btn btn-large btn-success" href="/poll/create">New Poll</a>
-      </div>
+  <div class="jumbotron">
+    <h1>Create a Poll!</h1>
+    <p class="lead">Create a new public poll.</p>
+    <a class="btn btn-large btn-success" href="/poll/create">New Poll</a>
+  </div>
 
+  <hr>
+    
+    <div id="recent-polls">
+        <ul class="list">
+            <li class="reference poll hidden">
+                <a class="string small" href="">This is the reference...</a>
+            </li>
+        </ul>
+    </div>
+@stop
+@section('javascripts')
+<script type="text/javascript">
+    
+    $(document).ready(function() {
+        
+        var pollRegistry = new POLL.REGISTRY.PollRegistry(),
+            pollMarshaller = new POLL.MARSHALLER.PollMarshaller(),
+            
+            refreshData,
+            
+            refreshPolls,
+            refreshPollVisuals,
+            
+            pollLoadSuccess,
+            pollLoadError;
+        
+        refreshData = function () 
+        {
+            pollRegistry.searchRecent(4, pollLoadSuccess, pollLoadError);   
+        }
+    
+        refreshPolls = function (data) {
+            var polls = pollMarshaller.marshall(data);
+            refreshPollChoices(polls);
+        };
+        
+        refreshPollChoices = function (polls) {
+            
+            var poll,
+                pollPrompt,
+                address,
+                
+                pollList = $("#recent-polls"),
+                pollReference = pollList.find(".reference"),
+                nonReferencePolls = pollList.find(".poll").not(".reference"),
+                clone = null,
+            
+                i = 0;
+            
+            nonReferencePolls.remove();
+            
+            for(i = 0; i < polls.length; i += 1)
+            {
+                poll = polls[i];
+                pollPrompt = poll.getPrompt();
+                pollIdentifier = poll.getIdentifier();
+
+                address = ('/poll/'+pollId);
+                
+                clone = pollReference.clone(true);
+                clone.toggleClass("hidden").toggleClass("reference");
+                clone.find(".string").text(pollPrompt).attr('href',address);
+                
+                pollList.append(clone);
+            }
+        };
+    
+        pollLoadSuccess = function (data) {
+            refreshPolls(data);  
+        };
+
+        pollLoadError = function () {
+            $("#recent-polls").toggleClass("hidden");
+        };
+    
+    });
+    
+</script>
+        
       <hr>
 
       <div class="row-fluid marketing">
